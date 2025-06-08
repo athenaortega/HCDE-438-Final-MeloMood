eaa72bed7bf0f5c12a3278d7ad9d06c9590cb58e
athenaortega <160282703+athenaortega@users.noreply.github.com>
2025-06-07 23:49:05 -0700

    Fixed deploying issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
 // This is the main jsx file which helps with the router components of the application.
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './contexts/AuthContext';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
